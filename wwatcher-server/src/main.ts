441a272551a4509e704099b68d0e3b4b1128b69c
k-sai-deekshith <143835968+k-sai-deekshith@users.noreply.github.com>
2024-08-12 13:58:39 -0700

    changes

--- wwatcher-server/src/main.ts
+++ wwatcher-server/src/main.ts
@@ -1,9 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+
+const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  console.log('In main.tsx');
   app.enableCors();
-  await app.listen(3000);
+
+  if (process.env.NODE_ENV !== 'production') {
+    await app.listen(3000, () => {
+      console.log('Server is running on http://localhost:3000');
+    });
+  } else {
+    await app.init();
+    console.log('App initialized');
+  }
 }
+
 bootstrap();
+
+export default server;
