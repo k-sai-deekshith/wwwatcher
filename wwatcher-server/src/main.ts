0958608d1036dd2e5badebd7ac4b8cee21b7be2a
k-sai-deekshith <143835968+k-sai-deekshith@users.noreply.github.com>
2024-08-12 19:19:21 -0700

    enable cors

--- wwatcher-server/src/main.ts
+++ wwatcher-server/src/main.ts
@@ -5,8 +5,12 @@ async function bootstrap() {
   console.log('Initializing Nest Application...');
 
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(process.env.PORT,'0.0.0.0');
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+  await app.listen(process.env.PORT);
 }
 
 bootstrap();
