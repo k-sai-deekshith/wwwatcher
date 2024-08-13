8ebcaa83feb13e5fe44f790f24e6616227ff1b86
k-sai-deekshith <143835968+k-sai-deekshith@users.noreply.github.com>
2024-08-12 18:17:38 -0700

    removed vercel changes

--- wwatcher-server/src/main.ts
+++ wwatcher-server/src/main.ts
@@ -1,34 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
-
-const server = express();
 
 async function bootstrap() {
   console.log('Initializing Nest Application...');
 
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  const app = await NestFactory.create(AppModule);
   app.enableCors();
-
-  // Log all registered routes
-  const httpServer = app.getHttpAdapter().getInstance();
-  httpServer._router.stack.forEach((layer) => {
-    if (layer.route) {
-      console.log(`Registered route: ${layer.route.path}`);
-    }
-  });
-
-  if (process.env.NODE_ENV !== 'production') {
-    await app.listen(3000, () => {
-      console.log('Server is running on http://localhost:3000');
-    });
-  } else {
-    await app.init();
-    console.log('App initialized in production mode');
-  }
+  await app.listen(process.env.PORT,'0.0.0.0');
 }
 
 bootstrap();
-
-export default server;
